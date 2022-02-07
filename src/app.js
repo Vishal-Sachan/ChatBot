149cdbf1bf3f37c31e595d65a229e70d7393a7c4
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-02-07 16:48:49 +0530

    Chatbot Created

---
+++ src/app.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+var prompt_sync = require("prompt-sync");
+var prompt = prompt_sync();
+
+var data = fs.readFileSync("../intents/question.json", "utf-8");
+data = JSON.parse(data);
+
+var exitData = fs.readFileSync("../intents/greetings_bye.json", "utf-8");
+exitData = JSON.parse(exitData);
+
+console.log("ChatBot Starting...");
+console.log("You can start chatting now");
+//console.log(exitData);
+
+do {
+    var input = prompt("> ");
+    var inputToCheck = input.toLowerCase();
+    //console.log(data.intents);
+    var result = data.intents.find((intent) => {
+        return (inputToCheck.localeCompare(intent.question) === 0)
+    })
+    console.log(result.answer);
+} while (input != 'exit')
+
+
+
+
+
+// function chatBot(input) {
+//     var inputToCheck = input.toLowerCase();
+//     exitData.questions.map((ques) => {
+//         if (inputToCheck.localeCompare(ques) === 0) {
+//             return exitData.answers;
+//         }
+//     });
+//     data.questions.map((ques) => {
+//         if (inputToCheck.localeCompare(ques) === 0) {
+//             return data.answers;
+//         }
+//     });
+// }
+// chatBot(input);
